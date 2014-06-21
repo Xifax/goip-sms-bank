ef2b70a6fa181e269217dd3ebdb6c2e5f1a92e30
Artiom Basenko <demi.log@gmail.com>
2014-06-21 17:06:52 +0400

    Multithreading example.

--- smsbank/apps/hive/utils.py
+++ smsbank/apps/hive/utils.py
@@ -1,16 +1,43 @@
 # encoding: utf-8
 import SocketServer
+import threading
 
 
 class HandleUdp(SocketServer.BaseRequestHandler):
     """
-    Process basic UDP requests.
+    Process basic UDP requests using single handle.
     """
 
     def handle(self):
         # Get data and socket
         data, socket = self.request[0].strip(), self.request[1]
+
         # Display received data
         print "%s send us: %s" % (self.client_address[0], data)
         # Reply with uppercased data
         socket.sendto(data.upper(), self.client_address)
+
+
+class ThreadedHandleUdp(SocketServer.BaseRequestHandler):
+    """
+    Process UDP requests using multiple instances of this handle.
+    """
+
+    def handle(self):
+        # Save current thread
+        thread = threading.current_thread()
+        # Get data and socket
+        data, socket = self.request[0].strip(), self.request[1]
+
+        # Display received data
+        print "[%s]%s send us: %s" % (
+            thread.name,
+            self.client_address[0],
+            data
+        )
+        # Reply with uppercased data
+        socket.sendto(data.upper(), self.client_address)
+
+
+class MultiServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
+    pass
--- smsbank/common/management/commands/udp_server.py
+++ smsbank/common/management/commands/udp_server.py
@@ -1,9 +1,14 @@
 # encoding: utf-8
 from django.core.management.base import BaseCommand
 
-from smsbank.apps.hive.utils import HandleUdp
+from smsbank.apps.hive.utils import (
+    HandleUdp,
+    ThreadedHandleUdp,
+    MultiServer
+)
 
 from optparse import make_option
+import threading
 import SocketServer
 
 
@@ -21,15 +26,34 @@ class Command(BaseCommand):
             default=1234,
             type=int,
             help='Port to serve on'),
+        make_option(
+            '--multithread',
+            action='store_true',
+            dest='multithread',
+            default=False,
+            help='Use multithreading'),
     )
 
-    help = 'UDP server for testing purposes'
+    help = """
+    UDP server for testing purposes. You may test it using:
+        nc -u <hostname> <port> << <data>
+    """
 
     def handle(self, *args, **options):
         """
         Launch UDP server
         """
-        SocketServer.UDPServer(
-            (options['host'], options['port']),
-            HandleUdp
-        ).serve_forever()
+        if options['multithread']:
+            server = MultiServer(
+                (options['host'], options['port']),
+                ThreadedHandleUdp
+            )
+
+            server_thread = threading.Thread(target=server.serve_forever)
+            server_thread.start()
+
+        else:
+            SocketServer.UDPServer(
+                (options['host'], options['port']),
+                HandleUdp
+            ).serve_forever()
