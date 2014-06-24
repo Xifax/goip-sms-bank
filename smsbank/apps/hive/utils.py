0cd2721388be90ecf11ca6e154289e37643c6c1a
Shido <Shido@192.168.0.141>
2014-06-24 06:25:37 +0400

    Added some of the code of UDP Server

--- smsbank/apps/hive/utils.py
+++ smsbank/apps/hive/utils.py
@@ -1,64 +1,129 @@
 # encoding: utf-8
-import SocketServer
-import threading
+import SocketServer as ss
+import string
+#import threading
+import re
+from __builtin__ import Exception
+import multiprocessing as mp
+import os
+import sys
+from _ast import Del
 
-#from twisted.internet import protocol
+port = 44444
+host = "0.0.0.0"
 
-################
-# SocketServer #
-################
+#commQueue = mp.A
 
-
-class HandleUdp(SocketServer.BaseRequestHandler):
+class GoipUDPListener(ss.BaseRequestHandler):
     """
-    Process basic UDP requests using single handle.
-    """
-
-    def handle(self):
-        # Get data and socket
-        data, socket = self.request[0].strip(), self.request[1]
-
-        # Display received data
-        print "%s send us: %s" % (self.client_address[0], data)
-        # Reply with uppercased data
-        socket.sendto(data.upper(), self.client_address)
-
-
-class ThreadedHandleUdp(SocketServer.BaseRequestHandler):
-    """
-    Process UDP requests using multiple instances of this handle.
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
     """
 
     def handle(self):
-        # Save current thread
-        thread = threading.current_thread()
-        # Get data and socket
-        data, socket = self.request[0].strip(), self.request[1]
+        data = self.request[0].strip()
+        socket = self.request[1]
+        command = self.parseRequest(self.request[0])
+        #print "{} wrote:".format(self.client_address[0])
+        #print data
+        #command = self.getCommand(data)
+        #print command
+        responce = ''
+        '''
+        if command in ['req', 'CGATT', 'CELLINFO', 'STATE', 'EXPIRY']:
+            responce = processServiceRequest(command, data)
+        #print "I answered"
+        #print responce
+        '''
+        socket.sendto(responce, self.client_address)
+        
+    def getCommand(self, data):
+        newdata = re.search('^([a-zA-Z]+)', data)
+        return newdata.group(0)
+        
+    def parseRequest(self, data):
+        reqdata = string.split(data, ";")
+        command = {}
+        for comBun in reqdata:
+            if string.find(comBun, ":") != -1:
+                tmp = string.split(comBun,":")
+                if tmp[0] == 'password':    #correcting for Chinese protocol unevenness, when sometimes its 'pass' and sometimes its 'password' 
+                    tmp[0] = 'pass'
+                command[tmp[0]] = tmp[1]
+                
+        return command
+        
+    def authDevice(self, command):
+        '''
+        Must check existence of such device id in DB and check password afterwards
+        '''
+        #Pseudocode:
+        #dev = getDeviceById(id)
+        #if command['id'] == dev.devId:
+        #    if command['pass'] == dev.pass:
+        #        return True
+        #return False
+        if command['pass'] == '123':
+            return True
 
-        # Display received data
-        print "[%s]%s send us: %s" % (
-            thread.name,
-            self.client_address[0],
-            data
-        )
-        # Reply with uppercased data
-        socket.sendto(data.upper(), self.client_address)
-
-    def finish(self):
-        pass
-
-
-class MultiServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
-    daemon_threads = True
-
-
-###########
-# Twisted #
-###########
-'''
-class Echo(protocol.DatagramProtocol):
-
-    def datagramReceived(self, data, (host, port)):
-        print "received %r from %s:%d" % (data, host, port)
-        self.transport.write(data, (host, port))
-'''
+class deviceWorker:
+    devid = None
+    expire = None
+    cgatt = None
+    imei = None
+    num = None
+    voipStatus = None
+    voipState = None
+    imsi = None
+    iccid = None
+    cells = None
+    gsm = None
+    signal = None
+    expiryTime = None
+    killFlag = False
+    password = None
+    
+    host = None
+    port = None
+    
+    queueIn = mp.Queue()
+    queueOut = mp.Queue()
+    
+    def __init__(self, devid):
+        self.devid = devid
+        self.queueIn = mp.Queue()
+        self.queueOut = mp.Queue()
+        return True
+        
+    def poll(self):
+        '''
+        Main worker function
+        '''
+        while True:
+            if not self.queueIn.empty():
+                self.processRequest()
+            if self.killFlag:
+                return
+            
+    def processRequest(self):
+        data = self.queueIn.get_nowait()
+        command = self.parseRequest(data)
+        
+        
+    def parseRequest(self, data):
+        reqdata = string.split(data, ";")
+        command = {}
+        for comBun in reqdata:
+            if string.find(comBun, ":") != -1:
+                tmp = string.split(comBun,":")
+                command[tmp[0]] = tmp[1]
+        return command
+    
+if __name__ == "__main__":
+    
+    worker = deviceWorker(1)
+    worker.queueIn.put_nowait("req:1")
+    
+    mp.Process(target=deviceWorker, args=(1,))
