f5277502f7baa6c5cf995a870c3ea49d6aec42f9
Shido <Shido@192.168.0.141>
2014-06-25 11:58:28 +0400

    Code cleanup Now using mp.Process overloading instead of invoking

--- smsbank/apps/hive/utils.py
+++ smsbank/apps/hive/utils.py
@@ -2,20 +2,18 @@
 import SocketServer as ss
 import socket
 import string
-#import threading
 import re
 from __builtin__ import Exception
 import multiprocessing as mp
 import os
 import sys
 from time import sleep
-#from _ast import Del
 
 port = 44444
 host = "0.0.0.0"
 
 #commQueue = mp.A
-class GoipUDPSender:
+class GoipUDPSender(mp.Process):
     """
     """
     socket = None
@@ -24,16 +22,16 @@ class GoipUDPSender:
     def __init__(self, queue):
         self.socket = socket
         self.queue = queue
-        self.sendResponces()
+        #self.sendResponces()
         
-    def sendResponces(self):
+    def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print "Colonel Listener is ready for Action!"
+        print "Colonel Sender is ready for Action!"
         while True:
             if not self.queue.empty():
                 while not self.queue.empty():
                     data = self.queue.get()
-                    print data
+                    print data['data']
                     sock.sendto(data['data'], data['host'])
             else:
                 sleep(1)
@@ -56,11 +54,12 @@ class GoipUDPListener(ss.BaseRequestHandler):
         socket = self.request[1]
         command = self.getCommand(self.request[0])
         query = self.parseRequest(self.request[0])
+        print self.devPool
         if not self.deviceActive(query['id']):
             if self.authDevice(query['id'], query['pass']):
                 queue = mp.Queue()
-                device = mp.Process(target=deviceWorker, args=(query['id'], self.client_address, queue, senderQueue))
-                #device = deviceWorker(query['id'])
+                #device = mp.Process(target=deviceWorker, args=(query['id'], self.client_address, queue, senderQueue))
+                device = deviceWorker(query['id'], self.client_address, queue, senderQueue)
                 device.start()
                 #device._target.newRun()
                 self.devPool[query['id']] = {}
@@ -71,27 +70,12 @@ class GoipUDPListener(ss.BaseRequestHandler):
         queue.put_nowait(query)
         print "queue " + str(queue.qsize())
         print "Process count: " + str(len(self.devPool))
-        
-            
-        #print "{} wrote:".format(self.client_address[0])
-        #print data
-        #command = self.getCommand(data)
-        #print command
-        responce = ''
-        '''
-        if command in ['req', 'CGATT', 'CELLINFO', 'STATE', 'EXPIRY']:
-            responce = processServiceRequest(command, data)
-        #print "I answered"
-        #print responce
-        '''
-        #socket.sendto(responce, self.client_address)
     
     def deviceActive(self, devId):
         if devId in self.devPool:
             return True
         return False
-        
-    
+  
     def getCommand(self, data):
         newdata = re.search('^([a-zA-Z]+)', data)
         return newdata.group(0)
@@ -116,9 +100,9 @@ class GoipUDPListener(ss.BaseRequestHandler):
             return True
         return False
 
-class deviceWorker:
+class deviceWorker(mp.Process):
     devid = None
-    expire = None
+    expire = int(20)
     cgatt = None
     imei = None
     num = None
@@ -140,30 +124,35 @@ class deviceWorker:
     queueOut = None
     
     def __init__(self, devid, host, queue, outQueue):
-#        mp.Process.__init__(self)
+        mp.Process.__init__(self)
         self.devid = devid
         self.queueIn = queue
         self.host = host
         self.queueOut = outQueue 
         print host
-        #self.queueOut = mp.Queue()
         print "mein Konstruktor: " + str(self.devid)
-        self.newRun()
-        #return True
+        #self.newRun()
         
-    def newRun(self):
+    
+        
+    def run(self):
         '''
         Main worker function
         '''
         print "OMG! I'm Running wild and free!"
+        self.expire = 20
         while True:
             if not self.queueIn.empty():
                 self.processRequest()
             else:
                 sleep(1)
-            print "from ps queue size is " + str(self.queueIn.qsize())
-            #if self.killFlag:
-            #    return
+                self.expire -= 1
+                print "Expire is now: " + str(self.expire)
+                
+            if self.expire <= 0:
+                print "For thy Emperor of the catkind i will sacrifice myself"
+                #return
+                #self.terminate()
             
     def processRequest(self):
         responce = {}
@@ -190,12 +179,8 @@ class deviceWorker:
             print cells
             self.cells = cells
         elif data['command'] == 'STATE':
-            #self.stateWrite(commandData)
             None
-            #print "Muich state"
         elif data['command'] == 'EXPIRY':
-            #self.expiryWrite(commandData)
-            #print "Much expiry"
             self.expire = data['exp']
         else:
             raise Exception
@@ -205,15 +190,10 @@ class deviceWorker:
 
     
 if __name__ == "__main__":
-    '''
-    worker = deviceWorker(1)
-    worker.queueIn.put_nowait("req:1")
-    
-    mp.Process(target=deviceWorker, args=(1,))
-    '''
     HOST, PORT = "0.0.0.0", 44444
     senderQueue = mp.Queue()
-    sender  = mp.Process(target=GoipUDPSender, args=(senderQueue,))
+    #sender  = mp.Process(target=GoipUDPSender, args=(senderQueue,))
+    sender = GoipUDPSender(senderQueue,)
     sender.start()
     
     server = ss.UDPServer((HOST, PORT), GoipUDPListener)
